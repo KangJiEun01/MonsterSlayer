25c3c86ddda6290eb02d1a6faf453bae0f110e92
MSM\sm990627 <sm990627@gmail.com>
2023-07-21 09:46:33 +0900

    no message

---
+++ Assets/HelperAI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelperAI : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

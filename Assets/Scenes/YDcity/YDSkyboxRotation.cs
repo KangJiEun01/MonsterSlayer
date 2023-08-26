da8ffbfdb181fe713865c8cb21c001041d74e911
DaMYE91 <yedam0901@gmail.com>
2023-08-27 01:13:14 +0900

    네온간판수정, 사막 배경 완료(배경 오브젝트, 풀떼기 배치 전)

---
+++ Assets/Scenes/YDcity/YDSkyboxRotation.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YDSkyboxRotation : MonoBehaviour
+{
+    public float skyboxRotationSpeed = 1.0f;
+
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * skyboxRotationSpeed);
+    }
+}
+
---
+++ Assets/Scenes/YDcity/YdRotationCloud.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YdRotationCloud : MonoBehaviour
+{
+    public float rotationSpeed = 30.0f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
+

ccab3bc1825d5cf89c948a18a10b9af6f89024ca
DESKTOP-JDNI5J5\KANGJI <vkdlsxm@naver.com>
2023-08-27 21:19:35 +0900

    코뿔소 UI 애니메이션 추가

--- Assets/Scripts/JiEun/Ui/RhinoHPbar.cs
+++ Assets/Scripts/JiEun/Ui/RhinoHPbar.cs
@@ -12,7 +12,7 @@ public class RhinoHPbar : MonoBehaviour
         // 오브젝트에 따른 HP Bar 위치 이동
         //m_goHpBar.transform.position = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, 0.8f, 0));
        HpBar.transform.position =new Vector3( Rhino.transform.position.x, Rhino.transform.position.y + 2f, Rhino.transform.position.z);
-       detection.transform.position = new Vector3(Rhino.transform.position.x, Rhino.transform.position.y + 2.7f, Rhino.transform.position.z);
+       detection.transform.position = new Vector3(Rhino.transform.position.x, Rhino.transform.position.y + 2.6f, Rhino.transform.position.z);
     }
 
 }

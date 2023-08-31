de26580a04dbcfd0d287137b6445a30282dfa495
K02\admin <vkdlsxm@naver.com>
2023-08-31 13:13:30 +0900

    씬연결중

---
+++ Assets/Scripts/JiEun/Ui/ButtonControll.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonControll : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void ClickStartButton()
+    {
+        SceneManager.LoadScene("MapSelect");
+    }
+    public void ClickbackButton() 
+    {
+        SceneManager.LoadScene("GameStart");
+    }
+    public void ClickLevelMapButton()
+    {
+        SceneManager.LoadScene("YDNeonCity");
+    }
+}

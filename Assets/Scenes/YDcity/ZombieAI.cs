bde25be78b32306dd058cfd162a679408492d2d7
K03\admin <yedam0901@gmail.com>
2023-09-25 10:09:13 +0900

    좀비 수정^-^ 커밋

--- Assets/Scenes/YDcity/ZombieAI.cs
+++ Assets/Scenes/YDcity/ZombieAI.cs
@@ -8,9 +8,9 @@ public class ZombieAI : MonoBehaviour
     private Animator animator; 
     private NavMeshAgent navMeshAgent; 
     private int currentWaypointIndex = 0;
-    private float attackRange = 1.0f; // 공격 범위
+    private float attackRange = 2.0f; // 공격 범위
     private float chaseRange = 10.0f; // 추적 범위
-    private float returnRange = 11.0f; // 되돌아가는 범위
+    private float returnRange = 1.0f; // 되돌아가는 범위
     private float idleDuration = 1.0f; // Idle 상태 지속
     private CharacterState characterState = CharacterState.Idle;
 
@@ -43,9 +43,11 @@ public class ZombieAI : MonoBehaviour
         // 추적 범위 내에 플레이어가 있을 때
         if (Vector3.Distance(transform.position, player.position) <= chaseRange)
         {
+            Debug.Log(Vector3.Distance(transform.position, player.position));
             // 공격 범위 내에 플레이어가 있을 때
             if (Vector3.Distance(transform.position, player.position) <= attackRange)
             {
+
                 SetCharacterState(CharacterState.Attack);
                 AttackPlayer(); // 플레이어를 공격하는 함수
             }
@@ -67,8 +69,16 @@ public class ZombieAI : MonoBehaviour
             }
             else
             {
-                // 서있기 ? ??
+                if (characterState == CharacterState.Idle) return;
+                // 서있기 ? ?? 
                 SetCharacterState(CharacterState.Idle);
+                currentWaypointIndex++;
+                if(wayPointGroup.IsValid(currentWaypointIndex) == false)
+                {
+                    currentWaypointIndex = 0;
+                }
+                navMeshAgent.SetDestination(wayPointGroup.GetPoint(currentWaypointIndex));
+                //SetCharacterState(CharacterState.Idle);
             }
         }
     }
@@ -91,7 +101,7 @@ public class ZombieAI : MonoBehaviour
                     break;
                 case CharacterState.Run:
                     animator.SetTrigger("Run");
-                    navMeshAgent.speed = 3.0f; //속도
+                    navMeshAgent.speed = 3.5f; //속도
                     break;
                 case CharacterState.Attack:
                     animator.SetTrigger("Attack");

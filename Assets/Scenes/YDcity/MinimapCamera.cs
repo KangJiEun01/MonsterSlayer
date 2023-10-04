b46b79095ff782fa5336fdcecfd46cbceb7aecb6
DaMYE91 <yedam0901@gmail.com>
2023-10-04 21:52:24 +0900

    미니맵을 넣었다. 문제점 : 아이콘 표시가 안된다(플레이어 인식을 못함)

---
+++ Assets/Scenes/YDcity/MinimapCamera.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour
+{
+    public Transform target;
+    public float offsetRatio;
+
+    Camera cam;
+    Vector2 size;
+    public Transform indicator;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        size = new Vector2(cam.orthographicSize, cam.orthographicSize * cam.aspect);
+    }
+
+    void Update()
+    {
+        Vector3 targetForwardVector = target.forward;
+        targetForwardVector.y = 0;
+        targetForwardVector.Normalize();
+
+        Vector3 position = new Vector3(target.transform.position.x, 1, target.transform.position.z)
+                           + targetForwardVector * offsetRatio * cam.orthographicSize;
+        transform.position = position;
+        transform.eulerAngles = new Vector3(90, 0, -target.eulerAngles.y);
+    }
+
+    public void ShowBorderIndicator(Vector3 position)
+    {
+        float reciprocal;
+        float rotation;
+        Vector2 distance = new Vector3(transform.position.x - position.x, transform.position.z - position.z);
+
+        distance = Quaternion.Euler(0, 0, target.eulerAngles.y) * distance;
+
+        if(Mathf.Abs(distance.x) > Mathf.Abs(distance.y))
+        {
+            reciprocal = Mathf.Abs(size.x / distance.x);
+            rotation = (distance.x > 0) ? 90 : -90;
+        }
+        else
+        {
+            reciprocal = Mathf.Abs(size.y / distance.y);
+            rotation = (distance.y > 0) ? 180 : 0;
+        }
+
+        indicator.localPosition = new Vector3(distance.x * -reciprocal, distance.y * -reciprocal, 1);
+        indicator.localEulerAngles = new Vector3(0, 0, rotation);
+    }
+
+    public void HideBorderIncitator()
+    {
+        indicator.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scenes/YDcity/MinimapSprite.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class MinimapSprite : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    public MinimapCamera minimapCamera;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(90, transform.parent.eulerAngles.y, 0);
+        transform.position = new Vector3(transform.parent.position.x, 0, transform.parent.position.z);
+
+        if(spriteRenderer.isVisible == false)
+        {
+            minimapCamera.ShowBorderIndicator(transform.position);
+        }
+        else
+        {
+            minimapCamera.HideBorderIncitator();
+        }
+    }
+}

f9084c9574259915b251a788becb4e28b4baca50
K03\admin <yedam0901@gmail.com>
2023-10-04 11:01:19 +0900

    예비 워프 텍스쳐 수정, 언더시티 포그 진하게 수정

---
+++ Assets/Scenes/YDcity/Dissolve.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class Dissolve : MonoBehaviour
+{
+    public VisualEffect VFXGraph;
+    public SkinnedMeshRenderer skinnedMesh;
+    public float dissolveRate = 0.0125f;
+    public float refreshRate = 0.025f;
+
+    private Material[] skinnedMaterials;
+
+
+    //이건 그냥 내가 애니메이션 넣고 싶어서 넣는것
+    //***R을 누르면 다시 살아나는 애니메이션 실행
+    //anim.SetTrigger("Rumba");
+    //R을 누를때는 VFX가 실행되지 않아야함
+
+    //[SerializeField] Animator anim;
+    [SerializeField] Animation anim;
+
+
+    void Start()
+    {
+        if (skinnedMesh != null)
+            skinnedMaterials = skinnedMesh.materials;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(DissolveCo());
+            anim.Play("death1");
+        }
+    }
+
+    IEnumerator DissolveCo()
+    {
+        if (VFXGraph != null)
+        {
+            VFXGraph.Play();
+        }
+
+        if (skinnedMaterials.Length > 0)
+        {
+            float counter = 0;
+
+            while (skinnedMaterials[0].GetFloat("_DissolveAmount") < 1)
+            {
+                counter += dissolveRate;
+                for (int i = 0; i < skinnedMaterials.Length; i++)
+                {
+                    skinnedMaterials[i].SetFloat("_DissolveAmount", counter);
+                }
+                yield return new WaitForSeconds(refreshRate);
+            }
+        }
+    }
+}

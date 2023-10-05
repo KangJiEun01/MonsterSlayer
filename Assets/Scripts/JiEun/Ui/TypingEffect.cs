0223eb486ed3ac6760ddbe30b831cbf89a93ff96
MSM_PC <sm990627@gmail.com>
2023-10-06 02:07:40 +0900

    튜토리얼 내용 살짝 수정

--- Assets/Scripts/JiEun/Ui/TypingEffect.cs
+++ Assets/Scripts/JiEun/Ui/TypingEffect.cs
@@ -9,15 +9,16 @@ public class TypingEffect : MonoBehaviour
     int maxNum = 0;
     string[] texts = {
         "인군 : 지금부터 필요한 조작법을 설명해 드리겠습니다.",
-        "Q/W/E/S 키로 이동할 수 있습니다.",
-        "마우스 우클릭으로 공격이 가능합니다.",
+        "W/A/S/D 키로 이동할 수 있습니다.",
+        "마우스 좌클릭으로 공격이 가능합니다.",
         "Shift 키를 누른 상태에서 이동을 하면 달려갑니다.",
         "Space 키를 누르면 점프합니다.",
         "E 키를 누르면 대쉬합니다.",
-        "R 키로 무기 교체가 가능합니다.",
-        "F 키로 회복 아이템을 사용해 주세요.",
-        "I 키로 인벤토리 창을 열 수 있습니다.",
-        "무기 교환창은 O 키를 눌러주십시오.",
+        "R 키를 누르면 재장전합니다.",
+        "F 키를 누르면 회복 아이템을 사용합니다.",
+        "I 키로 인벤토리 및 교환창을 열 수 있습니다.",
+        "재료를 모아 무기를 교환해 잠금 해제 할 수 있습니다.",
+        "O 키를 누르면 무기 교환창을 열 수있습니다.",
         "또 필요할 것이 있다면 저를 불러주세요. 무운을 빕니다."
     };
 
@@ -37,7 +38,7 @@ public class TypingEffect : MonoBehaviour
             yield return TypeText(currentText);
             yield return new WaitForSeconds(1f); // 다 타이핑 하고 1초
             // 글자 지움
-            if(textIndex == 10)
+            if(textIndex == texts.Length - 1)
             {
                 background.SetActive(false);
             }

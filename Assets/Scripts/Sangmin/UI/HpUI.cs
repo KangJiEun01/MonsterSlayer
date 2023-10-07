822e37422e457789fc251a716e61bce174d8c469
MSM_PC <sm990627@gmail.com>
2023-10-08 04:44:10 +0900

    hp바 수정

--- Assets/Scripts/Sangmin/UI/HpUI.cs
+++ Assets/Scripts/Sangmin/UI/HpUI.cs
@@ -4,13 +4,13 @@ using TMPro;
 
 public class HpUI : MonoBehaviour
 {
-    [SerializeField] Slider _hpBar;
+    [SerializeField] Image _hpBar;
     [SerializeField] TextMeshProUGUI _hpText;
     
     public void Init()
     {
         float _hp = GenericSingleton<PlayerCon>.Instance.HpStat;
-        _hpBar.value = _hp * 0.01f;
+        _hpBar.fillAmount = _hp * 0.01f;
         _hpText.text = _hp + "/100";
     }
 }

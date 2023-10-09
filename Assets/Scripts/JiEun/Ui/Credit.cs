23f80ebfcf52b43e6497f56de45a4e96928f7357
DESKTOP-JDNI5J5\KANGJI <vkdlsxm@naver.com>
2023-10-09 17:50:44 +0900

    메인씬 수정

--- Assets/Scripts/JiEun/Ui/Credit.cs
+++ Assets/Scripts/JiEun/Ui/Credit.cs
@@ -4,7 +4,7 @@ public class Credit : MonoBehaviour
 {
     Vector3 startPos = new Vector3(1200f, -500f, 0f);
     Vector3 ypos = new Vector3(0f, 1f, 0f);
-    float _speed = 40f;
+    float _speed = 80f;
     private void OnEnable()
     {
         transform.position = startPos;

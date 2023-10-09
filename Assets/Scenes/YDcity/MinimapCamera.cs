4a8c825f35b161dcf228327eb0467cdd9e6403a8
DaMYE91 <yedam0901@gmail.com>
2023-10-09 13:59:54 +0900

    미니맵이 왜 플레이어를 못찾을까..?

--- Assets/Scenes/YDcity/MinimapCamera.cs
+++ Assets/Scenes/YDcity/MinimapCamera.cs
@@ -11,49 +11,57 @@ public class MinimapCamera : MonoBehaviour
 
     void Start()
     {
-        indicator = GameObject.FindGameObjectWithTag("player").transform;
         target = GameObject.FindGameObjectWithTag("player").transform; // "Player" 태그를 가진 오브젝트를 찾아서 플레이어로 설정
         cam = GetComponent<Camera>();
         size = new Vector2(cam.orthographicSize, cam.orthographicSize * cam.aspect);
     }
 
-    void Update()
+    void LateUpdate()
     {
-        Vector3 targetForwardVector = target.forward;
-        targetForwardVector.y = 0;
-        targetForwardVector.Normalize();
+        if (target != null)
+        {
+            Vector3 targetForwardVector = target.forward;
+            targetForwardVector.y = 0;
+            targetForwardVector.Normalize();
 
-        Vector3 position = new Vector3(target.transform.position.x, 1, target.transform.position.z)
-                           + targetForwardVector * offsetRatio * cam.orthographicSize;
-        transform.position = position;
+            Vector3 position = new Vector3(target.transform.position.x, 1, target.transform.position.z)
+                               + targetForwardVector * offsetRatio * cam.orthographicSize;
+            transform.position = position;
+        }
     }
 
     public void RotateMinCam()
     {
-        transform.eulerAngles = new Vector3(90, 0, -target.eulerAngles.y);
+        if (target != null)
+        {
+            transform.eulerAngles = new Vector3(90, 0, -target.eulerAngles.y);
+        }
     }
 
     public void ShowBorderIndicator(Vector3 position)
     {
-        float reciprocal;
-        float rotation;
-        Vector2 distance = new Vector3(transform.position.x - position.x, transform.position.z - position.z);
-
-        distance = Quaternion.Euler(0, 0, target.eulerAngles.y) * distance;
-
-        if(Mathf.Abs(distance.x) > Mathf.Abs(distance.y))
+        if (target != null)
         {
-            reciprocal = Mathf.Abs(size.x / distance.x);
-            rotation = (distance.x > 0) ? 90 : -90;
-        }
-        else
-        {
-            reciprocal = Mathf.Abs(size.y / distance.y);
-            rotation = (distance.y > 0) ? 180 : 0;
-        }
+            float reciprocal;
+            float rotation;
+            Vector2 distance = new Vector3(transform.position.x - position.x, transform.position.z - position.z);
 
-        indicator.localPosition = new Vector3(distance.x * -reciprocal, distance.y * -reciprocal, 1);
-        indicator.localEulerAngles = new Vector3(0, 0, rotation);
+            distance = Quaternion.Euler(0, 0, target.eulerAngles.y) * distance;
+
+            if (Mathf.Abs(distance.x) > Mathf.Abs(distance.y))
+            {
+                reciprocal = Mathf.Abs(size.x / distance.x);
+                rotation = (distance.x > 0) ? 90 : -90;
+            }
+            else
+            {
+                reciprocal = Mathf.Abs(size.y / distance.y);
+                rotation = (distance.y > 0) ? 180 : 0;
+            }
+
+            indicator.localPosition = new Vector3(distance.x * -reciprocal, distance.y * -reciprocal, 1);
+            indicator.localEulerAngles = new Vector3(0, 0, rotation);
+        }
     }
 
     public void HideBorderIncitator()
